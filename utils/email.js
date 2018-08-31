7c083a0700603cb586bd30dc23e2abf08f9efe93
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-08-31 11:22:26 +0200

    added nodemailer

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,32 @@
-const axios = require('axios')
-var mailgun = require('mailgun-js')({
-    apiKey: '4f9452a48efb8164ea269f4912328b15-c1fe131e-03f09850',
-    domain: 'sandbox99abb5d62ba94b51aa60b4d2cbcf6df7.mailgun.org',
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'lukas.ironhack@gmail.com',
+        pass: 'ox99abb5d62ba94b51aa60b4d2',
+    },
 })
 
 function sendMail(to, subject, text, html) {
     return new Promise((resolve, reject) => {
         const data = {
-            from: 'postmaster@sandbox99abb5d62ba94b51aa60b4d2cbcf6df7.mailgun.org',
+            from: 'lukas.ironhack@gmail.com',
             to,
             subject,
             text,
             html,
         }
-        mailgun.messages().send(data, function(error, body) {
-            if (error) console.error(error)
-            console.log(body)
-            return resolve({ success: true })
-        })
+        transporter
+            .sendMail(data)
+            .then(info => {
+                console.log(info)
+                resolve({ success: true })
+            })
+            .catch(error => {
+                console.log(error)
+                resolve({ success: false })
+            })
     })
 }
 
